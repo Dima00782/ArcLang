9a15e3db32324a9767324c5f3a4e60476993a24d
Dmitry Bezhetskov <dima00782@gmail.com>
2018-03-30 00:47:28 +0700

    impl weak ref

--- src/main/java/com/github/dima00782/interpreter/ArcInterpreter.java
+++ src/main/java/com/github/dima00782/interpreter/ArcInterpreter.java
@@ -1,6 +1,7 @@
 package com.github.dima00782.interpreter;
 
 import com.github.dima00782.parser.Command;
+import com.github.dima00782.parser.Opcode;
 import javafx.util.Pair;
 
 import java.util.Arrays;
@@ -24,38 +25,46 @@ public class ArcInterpreter implements Interpreter {
         return new Pair<>(fields[fields.length - 1], object);
     }
 
+    private void sleep(int miliseconds) {
+        try {
+            Thread.sleep(miliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleAssignment(String lhs, String rhs, boolean isWeak) {
+        System.out.println((!isWeak ? "DEF_REF " : "DEF_WREF ") + lhs + " " + rhs);
+        Pair<String, ArcObject> lhsSearchResult = lookupObjectByName(lhs);
+        if ("object".equals(rhs) && !isWeak) {
+            lhs = lhsSearchResult.getKey();
+            ArcObject objectToInsert = lhsSearchResult.getValue();
+            objectToInsert.addFiled(lhs, new ArcObject());
+        } else {
+            lhs = lhsSearchResult.getKey();
+            ArcObject objectToInsert = lhsSearchResult.getValue();
+
+            Pair<String, ArcObject> rhsSearchResult = lookupObjectByName(rhs);
+            ArcObject rhsObject = rhsSearchResult.getValue();
+            rhs = rhsSearchResult.getKey();
+            ArcObject targetObject = rhsObject.getField(rhs);
+
+            if (!isWeak) {
+                targetObject.incrementRefCount();
+            }
+            objectToInsert.addFiled(lhs, targetObject);
+        }
+    }
+
     @Override
     public void run(Iterable<Command> commands) {
         for (Command command : commands) {
             switch (command.getOpcode()) {
-                case DEF_REF: {
-                    System.out.println("DEF_REF " + command.getArg(0) + " " + command.getArg(1));
-
+                case DEF_REF:
+                case DEF_WREF: {
                     String lhs = (String) command.getArg(0);
                     String rhs = (String) command.getArg(1);
-
-                    if ("object".equals(rhs)) {
-                        Pair<String, ArcObject> result = lookupObjectByName(lhs);
-                        lhs = result.getKey();
-                        ArcObject objectToInsert = result.getValue();
-                        objectToInsert.addFiled(lhs, new ArcObject());
-                    } else {
-                        Pair<String, ArcObject> lhsSearchResult = lookupObjectByName(lhs);
-                        lhs = lhsSearchResult.getKey();
-                        ArcObject objectToInsert = lhsSearchResult.getValue();
-
-                        Pair<String, ArcObject> rhsSearchResult = lookupObjectByName(rhs);
-                        ArcObject rhsObject = rhsSearchResult.getValue();
-                        rhs = rhsSearchResult.getKey();
-                        ArcObject targetObject = rhsObject.getField(rhs);
-
-                        targetObject.incrementRefCount();
-                        objectToInsert.addFiled(lhs, targetObject);
-                    }
-                    break;
-                }
-                case DEF_WREF: {
-                    System.out.println("DEF_WREF " + command.getArg(0) + " " + command.getArg(1));
+                    handleAssignment(lhs, rhs, command.getOpcode() == Opcode.DEF_WREF);
                     break;
                 }
                 case DEREF: {
@@ -81,21 +90,13 @@ public class ArcInterpreter implements Interpreter {
                 }
                 case SLEEP: {
                     System.out.println("SLEEP");
-                    try {
-                        Thread.sleep(SLEEP_INTERVAL);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    sleep(SLEEP_INTERVAL);
                     break;
                 }
                 case SLEEPR: {
                     System.out.println("SLEEPR");
                     int sleepTime = ThreadLocalRandom.current().nextInt(SLEEPR_RANGE_START, SLEEPR_RANGE_END);
-                    try {
-                        Thread.sleep(sleepTime);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    sleep(sleepTime);
                     break;
                 }
                 case DUMP: {
