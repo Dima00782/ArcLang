6e487e1445a5dc9711eaf086d340a2b184d5df48
Dmitry Bezhetskov <dima00782@gmail.com>
2018-03-29 11:43:42 +0700

    impl sleeps commands

--- src/main/java/com/github/dima00782/Interpreter/ArcInterpreter.java
+++ src/main/java/com/github/dima00782/Interpreter/ArcInterpreter.java
@@ -3,8 +3,13 @@ package com.github.dima00782.Interpreter;
 import com.github.dima00782.parser.Command;
 
 import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ArcInterpreter implements Interpreter {
+    private static final int SLEEP_INTERVAL = 100;
+    private static final int SLEEPR_RANGE_START = 10;
+    private static final int SLEEPR_RANGE_END = 100;
+
     @Override
     public void run(Iterable<Command> commands) {
         for (Command command : commands) {
@@ -29,10 +34,21 @@ public class ArcInterpreter implements Interpreter {
                 }
                 case SLEEP: {
                     System.out.println("SLEEP");
+                    try {
+                        Thread.sleep(SLEEP_INTERVAL);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 }
                 case SLEEPR: {
                     System.out.println("SLEEPR");
+                    int sleepTime = ThreadLocalRandom.current().nextInt(SLEEPR_RANGE_START, SLEEPR_RANGE_END);
+                    try {
+                        Thread.sleep(sleepTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 }
                 case DUMP: {
